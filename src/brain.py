3df3cf91a43387ed79c58ece4b351499eb1e8bfe
modestasgrz <modis.grz@gmail.com>
2023-05-08 15:35:18 +0300

    Init commit

---
+++ src/brain.py
@@ -0,0 +1,236 @@
+import networkx as netx
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
+from src.neurons import *
+
+class Brain(): # Directional graph
+  def __init__(self, genome, config):
+    self.connections = self.read_genome(genome, config.num_sensory_neurons, config.num_inner_neurons, config.num_action_neurons)
+    self.connections = self.remove_redundant_connections(self.connections)
+    self.brain = self.make_brain_dictionary(self.connections)
+    self.sense_neurons_mapping = config.sense_neurons_mapping
+
+  def __str__(self):
+    str_brain = "{" + "\n"
+    for key, value in self.brain.items():
+      str_connections = "{ \n"
+      for neuron, weight in value.items():
+        str_connections += "             " + str(neuron) + " :  " + str(weight) + ", \n"
+      str_connections += "          }"
+      str_brain += f"   {str(key)} : {str_connections}" + "\n"
+    str_brain += "}"
+    return str_brain
+  
+  # def forward(self, x): here initial x - dictionary of sense input values mapping
+  def __call__(self, sense_input): # functionality - debug this
+
+    # This is the where calculated values are accumulated
+    activated_sinks = []
+    x = 0.0
+    # Sensory Neurons activation
+    # Find Sensory Neurons and calculate activations,
+    # Then append them to the activated_sinks list
+    for source, sinks in self.brain.items():
+      if "SN" in str(source):
+        for sink, weight in sinks.items():
+          activation_input_type = self.sense_neurons_mapping.get(str(source), "") # Default get value = ""
+          x = sense_input.get(activation_input_type, 0.0) # Default get value = 0.0
+          x = source(x, weight)
+          activated_sinks.append((sink, x))
+
+    # print("ACTIVATED SINKS (IN):")
+    # print(np.array([(str(sink), value) for (sink, value) in activated_sinks]))
+    # print("")
+
+    # Inner Neurons activation
+    # Find some Inner Neuron
+    for new_source, new_x in activated_sinks:
+      if "IN" in str(new_source):
+        current_in = str(new_source)
+        # For every activation assigned to the chosen Inner Neuron collect input
+        in_input = [] 
+        for source, x in activated_sinks:
+          if str(source) == current_in:
+            in_input.append(x)
+        # Remove registered activation from the list - filter list, so it could not interfere with later runs
+        activated_sinks = [(node, activation) for (node, activation) in activated_sinks if str(node) != current_in]
+        # Now determine, to which new sinks assign this activation, if input exists
+        if len(in_input) > 0:
+          for source, sinks in self.brain.items():
+            if current_in == str(source):
+              # Calculate and assign new activations
+              for sink, weight in sinks.items():
+                x = source(in_input, weight)
+                activated_sinks.append((sink, x))
+
+    # print("ACTIVATED SINKS (AN):")
+    # print(np.array([(str(sink), value) for (sink, value) in activated_sinks]))
+    # print("")
+
+    # Action Neurons activation
+    # First declare output activations list
+    output_activations = []
+    # Find some Action Neuron - begining analogous to IN activation
+    for new_source, new_x in activated_sinks:
+      if "AN" in str(new_source):
+        current_an = str(new_source)
+        # For every activation assigned to the chosen Action Neuron collect input
+        an_input = [] 
+        for source, x in activated_sinks:
+          if str(source) == current_an:
+            an_input.append(x)
+        # Remove registered activation from the list - filter list, so it could not interfere with later runs
+        activated_sinks = [(node, activation) for (node, activation) in activated_sinks if str(node) != current_an]
+        # Now calculate activations for every action neuron and put them in a list, if inputs exist
+        if len(an_input) > 0:
+          x = new_source(an_input)
+          output_activations.append((new_source, x))
+
+    # print("OUTPUT ACTIONS (AN):")
+    # print(np.array([(str(sink), value) for (sink, value) in output_activations]))
+    # print("")
+
+    # Find index of the action neuron having the largest activation
+    _, an_index = np.argmax(np.array([(str(sink), value) for (sink, value) in output_activations]), axis=0)
+
+    # Formulate output
+    output = output_activations[an_index] # (action_neuron, activation)
+    action_neuron, activation = output
+    # print("SELECTED ACTION NEURON: " + str(action_neuron) + ", WITH ACTIVATION: " + str(activation))
+    return action_neuron.activate() # Provides number of in-game action - neuron's type id
+
+  def display_brain(self):
+    self.brain_graph = netx.DiGraph()
+    str_connections = [(str(source_neuron), str(sink_neuron), round(weight, 2)) for source_neuron, sink_neuron, weight in self.connections]
+    self.brain_graph.add_weighted_edges_from(str_connections)
+    colors = {}
+    for node in self.brain_graph.nodes():
+      if "SN" in node: # Sensory Neuron
+        colors[node] = "blue"
+      elif "IN" in node: # Inner Neuron
+        colors[node] = "grey"
+      else:
+        colors[node] = "red"
+    weight_labels = netx.get_edge_attributes(self.brain_graph, 'weight')
+    node_labels = {node:node for node in self.brain_graph.nodes()}
+    # Spring layout - appealing to the eye layout of a graph, required for draw function
+    pos = netx.spring_layout(self.brain_graph)
+    # netx.draw(self.brain_graph, with_labels=True)
+    netx.draw_networkx_nodes(self.brain_graph, pos, node_color=[colors.get(node, "gray") for node in self.brain_graph.nodes()], alpha=0.3)
+    netx.draw_networkx_edges(self.brain_graph, pos)
+    netx.draw_networkx_edge_labels(self.brain_graph, pos, edge_labels=weight_labels)
+    netx.draw_networkx_labels(self.brain_graph, pos, labels=node_labels, font_size=10)
+    plt.show()
+  
+  def make_brain_dictionary(self, connections): # make brain graph
+    brain = {}
+    for i, connection in enumerate(connections):
+      source_neuron, sink_neuron, weight = connection
+      appended = False
+      for existing_neuron in brain.keys():
+        if source_neuron.type_id == existing_neuron.type_id and str(source_neuron) == str(existing_neuron):
+          # Appends sink neuron to existing source neuron key
+
+          # Every neuron at init stage is assigned with a different reference,
+          # but neurons with the same type and id should match in reference
+          # as they are the same one neuron
+          source_neuron = existing_neuron
+          brain[source_neuron].append((sink_neuron, weight))
+          appended = True
+          break
+      if not appended:
+        # Adds new source neuron and corresponding sink neuron to it
+        brain[source_neuron] = [(sink_neuron, weight)]
+    for key, value in brain.items():
+      # Switching lists in values instead of dictionaries
+      brain[key] = {sink_neuron : weight for sink_neuron, weight in brain[key]}
+    
+    return brain
+
+  def remove_redundant_connections(self, connections):
+    # Make neuron list with num_inputs and num_outputs
+    neuron_list = [] # (neuron, num_inputs, num_outputs)
+    # print(np.array([(str(source), str(sink), weight) for (source, sink, weight) in connections]))
+    # print("")
+    for source, sink, weight in connections:
+      if str(source) == str(sink):
+        # Illegal connection - remove imediately
+        connections.remove((source, sink, weight))
+      source_updated = False
+      sink_updated = False
+      for i, item in enumerate(neuron_list):
+        neuron, num_inputs, num_outputs = item
+        if str(source) == str(neuron):
+          neuron_list[i] = (neuron, (num_inputs + 1), num_outputs)
+          source_updated = True
+        if str(sink) == str(neuron):
+          neuron_list[i] = (neuron, num_inputs, (num_outputs + 1))
+          sink_updated = True
+      if not source_updated:
+        neuron_list.append((source, 1, 0))
+      if not sink_updated:
+        neuron_list.append((sink, 0, 1))
+    
+    # Find redundant neurons
+    # Redundant neurons - INs with 0 inputs OR 0 outputs are redundant - connections with them will be removed - names of the neurons
+    redundant_neurons = [str(neuron) for (neuron, num_inputs, num_outputs) in neuron_list 
+                         if "IN" in str(neuron) and (num_inputs == 0 or num_outputs == 0)]
+
+    # Remove them from connections - filter connections
+    connections = [(source, sink, weight) for (source, sink, weight) in connections 
+                   if (not str(source) in redundant_neurons) and (not str(sink) in redundant_neurons)]
+    
+    # print(np.array([(str(source), str(sink), weight) for (source, sink, weight) in connections]))
+    # print("")
+    # print(np.array([neuron for neuron in redundant_neurons]))
+    return connections
+
+  def read_gene(self, gene, num_sensory_neurons, num_inner_neurons, num_action_neurons):
+    # Hex string to Bin string
+    gene = format(int(gene, 16), '032b')
+
+    # Parse encoded parameters from bits
+    # Source Type: 0 - sensory neuron; 1 - inner neuron
+    source_type = gene[0]
+    source_type = int(source_type, 2)
+
+    # Source ID: type id of the source neuron (without modulus applied)
+    source_id = gene[1:8]
+    source_id = int(source_id, 2)
+
+    # Sink Type: 0 - action neuron; 1 - inner neuron
+    sink_type = gene[8]
+    sink_type = int(sink_type, 2)
+    # NEW RULE: Inner Neurons cannot connect to themselves or each other
+    if source_type == 1:
+      sink_type = 0
+
+    # Sink ID: type id of the sink neuron (without modulus applied)
+    sink_id = gene[9:16]
+    sink_id = int(sink_id, 2)
+
+    # Weight: weight of the connection <- [-5 ... 5]
+    weight = gene[16:]
+    weight = (((int(weight, 2) / int("ffff", 16)) - 0.5) * 2) * 5 # [-5 .. 5]
+
+    if source_type == 1: # Inner neuron
+      source_id = source_id % num_inner_neurons
+      source_neuron = InnerNeuron(type_id=source_id)
+    elif source_type == 0: # Sensor neuron
+      source_id = source_id % num_sensory_neurons
+      source_neuron = SensorNeuron(type_id=source_id)
+
+    if sink_type == 1: # Inner neuron
+      sink_id = sink_id % num_inner_neurons
+      sink_neuron = InnerNeuron(type_id=sink_id)
+    elif sink_type == 0: # Action neuron
+      sink_id = sink_id % num_action_neurons
+      sink_neuron = ActionNeuron(type_id=sink_id)
+
+    return source_neuron, sink_neuron, weight
+
+  def read_genome(self, genome, num_sensory_neurons, num_inner_neurons, num_action_neurons):
+    return [self.read_gene(gene, num_sensory_neurons, num_inner_neurons, num_action_neurons) for gene in genome]
---
+++ src/neurons.py
@@ -0,0 +1,150 @@
+import numpy as np
+
+class SensorNeuron():
+  # Activation function produces value from 0.0 to 1.0
+  # in regards to the input and neuron's type
+  #
+  def __init__(self, type_id):
+    self.type_id = type_id
+    self.activation = {
+        0 : self.state, # SN_0 - state sensory neuron
+        1 : self.state, # SN_1 - state sensory neuron
+        2 : self.state, # SN_2 - state sensory neuron
+        3 : self.state, # SN_3 - state sensory neuron
+        4 : self.state, # SN_4 - state sensory neuron
+        5 : self.reward, # SN_6 - reward sensory neuron
+        6 : self.reward, # SN_7 - reward sensory neuron
+        7 : self.reward, # SN_8 - reward sensory neuron
+        8 : self.coins, # SN_9 - coins sensory neuron
+        9 : self.coins, # SN_10 - coins sensory neuron
+        10 : self.flag_get, # SN_11 - flag get sensory neuron
+        11 : self.flag_get, # SN_12 - flag get sensory neuron
+        12 : self.life, # SN_13 - life sensory neuron
+        13 : self.score, # SN_14 - score sensory neuron
+        14 : self.status, # SN_16 - status sensory neuron
+        15 : self.time, # SN_17 - time sensory neuron
+        16 : self.x_pos, # SN_18 - x position sensory neuron
+        17 : self.y_pos, # SN_19 - y position sensory neuron
+    }.get(type_id, self.no_action)
+
+  def __str__(self):
+    return f"SN_{self.type_id}"
+
+  def __call__(self, x, weight):
+    #return self.activation(x) * weight
+    return self.activation(x) * weight
+  
+  def state(self, x):
+    return x # If x - stadartized and normalized, will need to use softmax on state extraction nn
+
+  def reward(self, x):
+    # reward is clipped into range (-15; 15)
+    # making reward <- (0; 1) (regarding algorithm implementation video "sensory neurons provide value in a range (0; 1)")
+    return (x + 15.0) / 30.0
+
+  def coins(self, x):
+    # According to some unverified sources on internet - hard limit on coins that Mario can hold = 9999
+    # This number is used normalizing coins input, assuming that coin value can be in range (0; 9999)
+    return (x / 9999.0)
+
+  def flag_get(self, x):
+    # Flag value is either True (= 1) or False (= 0)
+    if x:
+      return 1.0
+    else:
+      return 0.0
+
+  def life(self, x):
+    # Maximum number of available lifes is a member of {3; 2; 1}
+    # Normalizing out of 3
+    return (x / 3.0)
+
+  def score(self, x):
+    # According to https://www.youtube.com/watch?v=Pnu72O4PREE&t=355s
+    # The highest score on Mario = 3,663,400 - should score value be standartized by this super high number?
+    # My concern is, whether this activation has any significant influece on the output of the network,
+    # when using this standartization
+    return (x / 3663400.0)
+
+  def status(self, x):
+    # Mario's status, i.e., {'small', 'tall', 'fireball'}
+    # 'tall' - better than small, fireball, I assume, as well
+    if x == 'small':
+      return 0.0
+    elif x == 'tall':
+      return 1.0
+    else:
+      return 1.0
+
+  def time(self, x):
+    # Time starts at 400
+    return (x / 400.0)
+
+  def x_pos(self, x):
+    # Without glitching through game mechanics, the maximum x could be 700 (very hardly of course)
+    return (x / 700.0)
+
+  def y_pos(self, x):
+    # Without glitching through game mechanics, the maximum y could be 400 (very hardly of course)
+    return (x / 400.0)
+
+  def no_action(self, x):
+    # No action selected for this neuron - action unindentified
+    return 0.0
+
+
+class InnerNeuron(): 
+  # Simple standard neuron - perceptron
+  def __init__(self, type_id):
+    self.type_id = type_id
+
+  def __str__(self):
+    return f"IN_{self.type_id}"
+
+  def __call__(self, x, weight):
+    # Here x is considered to be a list of values
+    return np.tanh(sum(x)) * weight
+
+  
+
+class ActionNeuron():
+  # Action neuron should provide the number of an action they trying to invoke
+  # This number at init matches neuron's type_id
+  #
+  # However, the dictionary of actions is presented here
+  #
+  # self.action = { # 4 bits are enough
+  #       0 : ['NOOP'],
+  #       1 : ['right'],
+  #       2 : ['right', 'A'],
+  #       3 : ['right', 'B'],
+  #       4 : ['right', 'A', 'B'],
+  #       5 : ['A'],
+  #       6 : ['left'],
+  #       7 : ['left', 'A'],
+  #       8 : ['left', 'B'],
+  #       9 : ['left', 'A', 'B'],
+  #       10 : ['down'],
+  #       11 : ['up'],
+  #   }
+  #
+  # self.__call__() method calculates the outcome of inputs, but does not activate the action
+  #
+  # Action is activated, when particular neuron is selected in brain
+  # by taking this neuron and calling self.activate() method on it
+  # self.activate() simply provides the number of the selected action
+  # which matches type_id of the neuron
+  #
+  # So, this neuron is a simple perceptron with additional, task-related, activate feature
+  #
+  def __init__(self, type_id):
+    self.type_id = type_id
+
+  def __str__(self):
+    return f"AN_{self.type_id}"
+
+  def __call__(self, x):
+    return np.tanh(sum(x))
+  
+  def activate(self):
+    return self.type_id
